3fd2a0477eb9ff630b5a8cc998fca988b8e2d334
Paulamago <paulamago@gmail.com>
2026-04-09 09:53:34 +0200

    fix: replace Supabase anon key with valid JWT

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://vqmowezlvkvmxrdauihv.supabase.co'
-const SUPABASE_KEY = 'sb_publisable_c4seBvEi5lrCduOyz4vK7A_-BpwuOnB'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxbW93ZXpsdmt2bXhyZGF1aWh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTkwODEsImV4cCI6MjA5MTI5NTA4MX0.RgbUgaIivdCwuX2RwqJMpvusNQ4XN1RxzH_ZeTOhEnk'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
